f5a717b21c1bbd59d0c83477e84bbdcdd46c6de6
eferistin <eferistin@gmail.com>
2018-10-03 12:36:24 -0400

    Test 1 pass insure each feed has url

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,13 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('Each feed in allFeeds object has url and not empty', function() {
+            allFeeds.forEach(function(feed){
+            expect(feed.url).toBeDefined();
+            expect(feed.url).not.toBe("");
+        
+        });
+     });
 
 
         /* TODO: Write a test that loops through each feed
